0809fa473bbce2e4f53d7786b3f8fe162eb66e18
shatheeszz <31099718+shatheeszz@users.noreply.github.com>
2025-11-20 12:03:59 -0800

    Add User model with Sequelize - Authentication and role management

---
+++ backend/models/User.js
@@ -0,0 +1,84 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [3, 50]
+        }
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM('admin', 'customer'),
+        defaultValue: 'customer'
+    },
+    phone: {
+        type: DataTypes.STRING(15),
+        allowNull: true
+    },
+    address: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    lastLogin: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    timestamps: true,
+    hooks: {
+        beforeCreate: async (user) => {
+            if (user.password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+        beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        }
+    }
+});
+
+// Instance method to compare password
+User.prototype.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Instance method to generate auth token
+User.prototype.generateAuthToken = function() {
+    const jwt = require('jsonwebtoken');
+    return jwt.sign(
+        { id: this.id, email: this.email, role: this.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    );
+};
+
+module.exports = User;
